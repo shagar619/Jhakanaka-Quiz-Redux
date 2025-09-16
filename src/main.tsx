c6a53ee2e8152de66d4eba549f41a6c2b57d2750
shagar619 <ashagar619@gmail.com>
2025-09-16 23:25:31 +0600

    added toggle button

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import { ThemeProvider } from './providers/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+
     <Provider store={store}>
       <App />
     </Provider>
+
+    </ThemeProvider>
+
   </StrictMode>,
 )
